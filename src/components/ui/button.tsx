60b5d54d3516321a4eada8c5be1267bd939d36f8
Famgz <famgz@proton.me>
2024-10-11 14:01:42 -0300

    fix: minor ui components style adjustments

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,14 +10,14 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/80',
+          'bg-primary text-primary-foreground shadow hover:bg-primary/70',
         destructive:
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-input bg-none shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border border-input bg-none shadow-sm hover:bg-background/60 hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost: 'hover:bg-background/60 hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
@@ -31,7 +31,7 @@ const buttonVariants = cva(
       variant: 'default',
       size: 'default',
     },
-  },
+  }
 );
 
 export interface ButtonProps
@@ -50,7 +50,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         {...props}
       />
     );
-  },
+  }
 );
 Button.displayName = 'Button';
 
